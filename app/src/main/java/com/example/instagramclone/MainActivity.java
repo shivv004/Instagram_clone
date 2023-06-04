62e7538ee0a0ee9ac36bcd43937d2246151d01b9
shivv004 <shiv.singh1104@gmail.com>
2023-06-04 21:26:13 +0530

    version 1.0

---
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView17;
+    private ImageView imageView18;
+    private ImageView imageView19;
+    private ImageView imageView;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
+    private ImageView imageView5;
+    private ImageView imageView6;
+    private ImageView imageView7;
+    private ImageView imageView8;
+    private ImageView imageView20;
+    private ImageView imageView21;
+    private ImageView imageView22;
+    private ImageView imageView23;
+    private ImageView imageView24;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Log.d("general", "onCreate: Working successfully!");
+        imageView17 = findViewById(R.id.imageView17);
+        imageView18 = findViewById(R.id.imageView18);
+        imageView19 = findViewById(R.id.imageView19);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView20 = findViewById(R.id.imageView20);
+        imageView21 = findViewById(R.id.imageView21);
+        imageView22 = findViewById(R.id.imageView22);
+        imageView23 = findViewById(R.id.imageView23);
+        imageView24 = findViewById(R.id.imageView24);
+        imageView17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageView24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "under development!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
